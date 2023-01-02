4af7d5c8d00cbeaab3ff0a73a88b82387178ebce
tminh1902 <minhanh190202@gmail.com>
2023-01-02 16:49:37 +0700

    done search nhưng còn bug to đùng 16h49 2/1/2023

--- src/page/user/Search-job.js
+++ src/page/user/Search-job.js
@@ -13,66 +13,75 @@ export default function SearchJob() {
     )
     return (
         <>
-            <div className="container-listJob">
-                <div className="row">
-                    <div className="col-9 main">
+            {job.length === 0 ? <div>khong tim thay ket qua</div> : job.map((item, index) => (
+                <>
+                    <div className="container-listJob">
                         <div className="row">
-                            <div className="col-5 card-job">
+                            <div className="col-9 main">
                                 <div className="row">
-                                    <div className="col-1">
-                                        <img src="https://iweb.tatthanh.com.vn/pic/3/blog/images/image(2068).png"
-                                             alt="logo" className="card-logo"/>
-                                    </div>
-                                    <div className="col-7" style={{marginLeft: "15px"}}>
-                                        <p className="job-description">Nhân viên tiếp tân khách sạn </p>
-                                        <p className="companyName">Công ty TNHH 1 thành viên</p>
-                                    </div>
-                                    <div className="col-3">
-                                        <img src="https://cdn-icons-png.flaticon.com/128/3199/3199306.png" alt=""
-                                             style={{
-                                                 height: '80px',
-                                                 width: '80px',
-                                                 objectFit: "cover",
-                                                 marginLeft: '30px',
-                                                 marginTop: '21px'
-                                             }}/>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-12">
-                                        <div className="card-description">
-                                            <div className="description"><img
-                                                src="https://cdn-icons-png.flaticon.com/128/2838/2838912.png" alt=""
-                                                className="icon-description"/>Hà Nội
+                                    <div className="col-5 card-job">
+                                        <div className="row">
+                                            <div className="col-1">
+                                                <img
+                                                    src="https://iweb.tatthanh.com.vn/pic/3/blog/images/image(2068).png"
+                                                    alt="logo" className="card-logo"/>
                                             </div>
-                                            <div className="description"><img
-                                                src="https://cdn-icons-png.flaticon.com/128/2454/2454282.png" alt=""
-                                                className="icon-description"/>VND 20.000.000 - 50.000.000
+                                            <div className="col-7" style={{marginLeft: "15px"}}>
+                                                <p className="job-description">{item.title} </p>
+                                                <p className="companyName">{item.name} </p>
                                             </div>
-                                            <div className="description"><img
-                                                src="https://cdn-icons-png.flaticon.com/128/639/639394.png" alt=""
-                                                className="icon-description"/>Từ 3-5 năm
+                                            <div className="col-3">
+                                                <img src={item.image}
+                                                     alt=""
+                                                     style={{
+                                                         height: '80px',
+                                                         width: '80px',
+                                                         objectFit: "cover",
+                                                         marginLeft: '30px',
+                                                         marginTop: '21px'
+                                                     }}/>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="row" style={{marginTop: '23px'}}>
-                                    <div className="col-12">
-                                        <p style={{fontSize: '12px'}}><img
-                                            src="https://cdn-icons-png.flaticon.com/128/2088/2088617.png" alt=""
-                                            style={{
-                                                width: '12px',
-                                                height: '12px',
-                                                objectFit: 'cover',
-                                                marginRight: '5px'
-                                            }}/>Thời gian ứng tuyển : 48 giờ </p>
+                                        <div className="row">
+                                            <div className="col-12">
+                                                <div className="card-description">
+                                                    <div className="description"><img
+                                                        src="https://cdn-icons-png.flaticon.com/128/2838/2838912.png"
+                                                        alt=""
+                                                        className="icon-description"/>{item.addressWork}
+                                                    </div>
+                                                    <div className="description"><img
+                                                        src="https://cdn-icons-png.flaticon.com/128/2454/2454282.png"
+                                                        alt=""
+                                                        className="icon-description"/>VND {item.wageStart} - {item.wageEnd}
+                                                    </div>
+                                                    <div className="description"><img
+                                                        src="https://cdn-icons-png.flaticon.com/128/639/639394.png"
+                                                        alt=""
+                                                        className="icon-description"/>{item.experience} năm
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row" style={{marginTop: '23px'}}>
+                                            <div className="col-12">
+                                                <p style={{fontSize: '12px'}}><img
+                                                    src="https://cdn-icons-png.flaticon.com/128/2088/2088617.png" alt=""
+                                                    style={{
+                                                        width: '12px',
+                                                        height: '12px',
+                                                        objectFit: 'cover',
+                                                        marginRight: '5px'
+                                                    }}/>Thời gian ứng tuyển : 48 giờ </p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </>
+            ))}
         </>
     )
 }
