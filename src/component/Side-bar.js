67d3ac0b9ea14d7c1eb5f855c76bcff836851384
hoangtran2004 <tranhuyhoang58825@gmail.com>
2023-01-09 14:24:14 +0700

    abc

--- src/component/Side-bar.js
+++ src/component/Side-bar.js
@@ -1,18 +1,29 @@
-import '../style/Side-bar.css'
+import '../../style/Side-bar.css'
 import {useLocation, useNavigate, useParams, useSearchParams,} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {editJob, getJob, searchJob} from "../service/Job-service";
-import {getCity} from "../service/City-service";
-import {getCategory} from "../service/Category-service";
+import {editJob, getJob, searchJob} from "../../service/Job-service";
+import {getCity} from "../../service/City-service";
+import {getCategory} from "../../service/Category-service";
+import '../../style/range.css'
 
 export default function SideBar() {
     let dispatch = useDispatch()
     let navigate = useNavigate()
     let search = useLocation().search.replace('?', '')
+    let [experience, setExperience] = useState([{experienceId: 0, nameExperience: '< 1 năm'}, {
+        experienceId: 1, nameExperience: '1-2 năm'
+    }, {experienceId: 2, nameExperience: '3- 5 năm'}, {
+        experienceId: 3, nameExperience: '> 5 năm'
+    }])
+    let [statusJob, setStatusJob] = useState([{idStatusTime: 0, nameStatus: 'Full time'}, {
+        idStatusTime: 1, nameStatus: 'Part time'
+    }])
     const queryParams = new URLSearchParams(search)
     let cityQuery = []
     let categoryQuery = []
+    let statusQuery = []
+    let expQuery = []
     useEffect(() => {
         dispatch(searchJob(search))
     }, [search])
@@ -29,29 +40,33 @@ export default function SideBar() {
         return map
     }
     let cities = useSelector((state) => {
-        console.log(state)
         return state.city.city
     })
     let categories = useSelector(state => {
         return state.category.category
     })
+    let queryPrice
+
+
     let checkQuery = (key) => {
         return getArrParams().filter((item) => {
             return item.key === `${key}`
         })
     }
-    if (checkQuery('address').length !== 0) {
-        cityQuery = checkQuery('address')[0].value.split(',')
-        console.log(cityQuery, 111111)
+    if (checkQuery('addressWork').length !== 0) {
+        cityQuery = checkQuery('addressWork')[0].value.split(',')
     }
     if (checkQuery("categoryId").length !== 0) {
         categoryQuery = checkQuery("categoryId")[0].value.split(',')
     }
+    if (checkQuery("statusTime").length !== 0) {
+        statusQuery = checkQuery("statusTime")[0].value.split(',')
+    }
+    if (checkQuery("experience").length !== 0) {
+        expQuery = checkQuery("experience")[0].value.split(',')
+    }
 
-    let handleSearch = async (e) => {
-        const checked = e.target.checked;
-        const key = e.target.name;
-        const value = e.target.value;
+    let handleSearch = async (checked, key, value) => {
         let newArrQuery = search.split('&')
         let index = -1
         let newQuery
@@ -88,15 +103,63 @@ export default function SideBar() {
 
     let printCheckbox = (name, value, nameCB, check, id) => {
         return (<>
-            <input checked={check} id={id} type={"checkbox"} name={name} value={value}
-                   onChange={(values) => handleSearch(values)} className={'check-box'}/>
-            <label htmlFor={id} className={'option'}>
+            <input className={'css-checkbox'} checked={check} id={id} type={"checkbox"} name={name} value={value}
+                   onChange={(values) => {
+                       let check = values.target.checked
+                       let key = values.target.name
+                       let value = values.target.value
+                       return handleSearch(check, key, value)
+                   }}/>
+            <label htmlFor={id}>
                 {nameCB}
             </label>
             <br/>
         </>)
     }
 
+    let handleRange = (check) => {
+        if (check) {
+            console.log(check)
+            const rangeInput = document.querySelectorAll(".range-input input"),
+                priceInput = document.querySelectorAll(".price-input input"),
+                progress = document.querySelector('.slider .progress');
+            let priceGap = 0.5;
+            rangeInput.forEach(input => {
+                input.addEventListener("input", (e) => {
+                    // get val range
+                    let minVal = parseInt(rangeInput[0].value), maxVal = parseInt(rangeInput[1].value);
+                    clearTimeout(queryPrice)
+                    if (maxVal - minVal < priceGap) {
+                        if (e.target.className === "range-min") {
+                            rangeInput[0].value = maxVal - priceGap
+                        } else {
+                            rangeInput[1].value = minVal + priceGap
+                        }
+                    } else {
+                        priceInput[0].value = minVal
+                        priceInput[1].value = maxVal
+                        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+                        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+                    }
+                    queryPrice = setTimeout(() => {
+                        return handleSearch(check, "wage", `${minVal},${maxVal}`)
+                    }, 1000)
+                })
+            })
+        } else {
+            const rangeInput = document.querySelectorAll(".range-input input"),
+                priceInput = document.querySelectorAll(".price-input input"),
+                progress = document.querySelector('.slider .progress');
+            let minVal = parseInt(rangeInput[0].value), maxVal = parseInt(rangeInput[1].value);
+            rangeInput[0].value = 0
+            rangeInput[1].value = 0
+            priceInput[0].value = 0
+            priceInput[1].value = 0
+            progress.style.left = 100 + "%";
+            progress.style.right = 0;
+            return handleSearch(check, "wage", `${minVal},${maxVal}`)
+        }
+    }
     return (<>
         <div className="container-sideBar" style={{marginTop: 90}}>
             {/*search category start*/}
@@ -122,10 +185,69 @@ export default function SideBar() {
             <div className="row">
                 <div className="col-12 type-job">
                     <p style={{marginLeft: "14px", fontSize: "14px"}}>Địa điểm làm viêc</p>
-                    {cities.map((item)=> (cityQuery.includes(`${item.cityId}`)? printCheckbox("address", `${item.cityId}`, `${item.nameCity}`, true, `${item.address}${item.nameCity}`):printCheckbox("address", `${item.cityId}`, `${item.nameCity}`, false, `${item.address}${item.nameCity}`)))}
+                    {cities.map((item) => (cityQuery.includes(`${item.cityId}`) ? printCheckbox('addressWork', `${item.cityId}`, `${item.nameCity}`, true, `${item.cityId}${item.nameCity}`) : printCheckbox('addressWork', `${item.cityId}`, `${item.nameCity}`, false, `${item.cityId}${item.nameCity}`)))}
                 </div>
             </div>
             {/*search address end*/}
+            {/*search by money start*/}
+            {/*<div className="row" style={{padding: '12px'}}>*/}
+            {/*    <div className="col-12">*/}
+            {/*        <hr/>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<div className="row">*/}
+            {/*    <div className="col-12 type-job">*/}
+            {/*        <span style={{marginLeft: "14px", fontSize: "14px"}}>Mức lương</span>*/}
+            {/*        <div className="switch">*/}
+            {/*            <input id="switch-1" type="checkbox" onClick={(event) => handleRange(event.target.checked)}*/}
+            {/*                   name={'price'}*/}
+            {/*                   className="switch-input"/>*/}
+            {/*            <label htmlFor="switch-1" className="switch-label">Switch</label>*/}
+            {/*        </div>*/}
+            {/*        <div className="wrapper col-12">*/}
+            {/*            <div className="price-input">*/}
+            {/*                <div className="filed">*/}
+            {/*                    <spap>Từ</spap>*/}
+            {/*                    <input type="number" className="input-min" value="0"/><span>tr</span>*/}
+            {/*                </div>*/}
+            {/*                <div style={{width: 40}}></div>*/}
+            {/*                <div className="filed">*/}
+            {/*                    <spap>đến</spap>*/}
+            {/*                    <input type="number" className="input-max" value="0"/><span>tr</span>*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*            <div className="slider">*/}
+            {/*                <div className="progress"></div>*/}
+            {/*            </div>*/}
+            {/*            <div className="range-input">*/}
+            {/*                <input type="range" className="range-min" min="0" max="50" step="1"/>*/}
+            {/*                <input type="range" className="range-max" min="0" max="50" step="1"/>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*    search by money end*/}
+            {/*loai cong viec*/}
+            <div className="row">
+                <div className="col-12 type-job">
+                    <p style={{marginLeft: "14px", fontSize: "14px"}}>Loại công việc</p>
+                    {statusJob.map((item) => (statusQuery.includes(`${item.idStatusTime}`) ? printCheckbox('statusTime', `${item.idStatusTime}`, `${item.nameStatus}`, true, `${item.idStatusTime}${item.nameStatus}`) : printCheckbox('statusTime', `${item.idStatusTime}`, `${item.nameStatus}`, false, `${item.idStatusTime}${item.nameStatus}`)))}
+                </div>
+            </div>
+            {/*loai cong viec*/}
+            {/*    kinh nghiệm*/}
+            <div className="row" style={{padding: '12px'}}>
+                <div className="col-12">
+                    <hr/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 type-job">
+                    <p style={{marginLeft: "14px", fontSize: "14px"}}>Loại công việc</p>
+                    {experience.map((item) => (expQuery.includes(`${item.experienceId}`) ? printCheckbox('experience', `${item.experienceId}`, `${item.nameExperience}`, true, `${item.experienceId}${item.nameExperience}`) : printCheckbox('experience', `${item.experienceId}`, `${item.nameExperience}`, false, `${item.experienceId}${item.nameExperience}`)))}
+                </div>
+            </div>
+            {/*    kinh nghiệm*/}
         </div>
     </>)
 }
--- src/page/user/All-job.js
+++ src/page/user/All-job.js
@@ -33,73 +33,73 @@ export default function AllJob() {
                             {jobs === undefined ? <></> :
                                 jobs.map((item) => {
 
-                                            return (
-                                                <div className="col-5 card-job" onClick={()=>{
-                                                    detailJob({id:item?.jobId})
-                                                }}>
-                                                    <div className="row">
-                                                        <div className="col-2">
-                                                            <img src={item.image} alt="" className={'card-logo'}/>
+                                        return (
+                                            <div className="col-5 card-job" onClick={() => {
+                                                detailJob({id: item?.jobId})
+                                            }}>
+                                                <div className="row">
+                                                    <div className="col-2">
+                                                        <img src={item.image} alt="" className={'card-logo'}/>
 
-                                        </div>
-                                        <div className="col-7" style={{marginLeft: "15px"}}>
-                                            <p className="job-description">{item.title} </p>
-                                            <p className="companyName">{item.name} </p>
-                                        </div>
-                                        <div className="col-3">
-
-                                                        </div>
                                                     </div>
-                                                    <div className="row">
-                                                        <div className="col-12">
-                                                            <div className="card-description">
-                                                                <div className="description"><img
-                                                                    src="https://cdn-icons-png.flaticon.com/128/3850/3850259.png"
-                                                                    alt=""
-                                                                    className="icon-description"/>{item?.nameCategory}
-                                                                </div>
-                                                                <div className="description"><img
-                                                                    src="https://cdn-icons-png.flaticon.com/128/2838/2838912.png"
-                                                                    alt=""
-                                                                    className="icon-description"/>{item?.nameCity}
-                                                                </div>
-                                                                {tokenUser ? <div className="description"><img
-                                                                    src="https://cdn-icons-png.flaticon.com/128/2454/2454282.png"
-                                                                    alt=""
-                                                                    className="icon-description"/>VND {formatter.format(item.wageStart)} - {formatter.format(item.wageEnd)}
-                                                                </div> : <></>}
+                                                    <div className="col-7" style={{marginLeft: "15px"}}>
+                                                        <p className="job-description">{item.title} </p>
+                                                        <p className="companyName">{item.name} </p>
+                                                    </div>
+                                                    <div className="col-3">
 
-                                                                <div className="description"><img
-                                                                    src="https://cdn-icons-png.flaticon.com/128/639/639394.png"
-                                                                    alt=""
-                                                                    className="icon-description"/>{item.experience}
-                                                                </div>
+                                                    </div>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-12">
+                                                        <div className="card-description">
+                                                            <div className="description"><img
+                                                                src="https://cdn-icons-png.flaticon.com/128/3850/3850259.png"
+                                                                alt=""
+                                                                className="icon-description"/>{item?.nameCategory}
+                                                            </div>
+                                                            <div className="description"><img
+                                                                src="https://cdn-icons-png.flaticon.com/128/2838/2838912.png"
+                                                                alt=""
+                                                                className="icon-description"/>{item?.nameCity}
+                                                            </div>
+                                                            {tokenUser ? <div className="description"><img
+                                                                src="https://cdn-icons-png.flaticon.com/128/2454/2454282.png"
+                                                                alt=""
+                                                                className="icon-description"/>VND {formatter.format(item.wageStart)} - {formatter.format(item.wageEnd)}
+                                                            </div> : <></>}
+
+                                                            <div className="description"><img
+                                                                src="https://cdn-icons-png.flaticon.com/128/639/639394.png"
+                                                                alt=""
+                                                                className="icon-description"/>{item.experience === 0 ? "Dưới một năm" : item.experience === 1 ? "Từ 1-3 năm" : item.experience === 2 ? "Từ 3-5 năm" : "Trên 5 năm"}
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <div className="row" style={{marginTop: '23px'}}>
-                                                        <div className="col-12">
-                                                            <p style={{fontSize: '12px'}}><img
-                                                                src="https://cdn-icons-png.flaticon.com/128/2088/2088617.png"
-                                                                alt=""
-                                                                style={{
-                                                                    width: '12px',
-                                                                    height: '12px',
-                                                                    objectFit: 'cover',
-                                                                    marginRight: '5px'
-                                                                }}/>Thời gian ứng tuyển : 48 giờ </p>
-                                                        </div>
+                                                </div>
+                                                <div className="row" style={{marginTop: '23px'}}>
+                                                    <div className="col-12">
+                                                        <p style={{fontSize: '12px'}}><img
+                                                            src="https://cdn-icons-png.flaticon.com/128/2088/2088617.png"
+                                                            alt=""
+                                                            style={{
+                                                                width: '12px',
+                                                                height: '12px',
+                                                                objectFit: 'cover',
+                                                                marginRight: '5px'
+                                                            }}/>Thời gian ứng tuyển : 48 giờ </p>
                                                     </div>
                                                 </div>
-                                            )
+                                            </div>
+                                        )
                                     }
                                 )
-                        })}
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>)
+        </div>)
 }
 
 
